cfd78badc5d64ee0a4f997ba21d34be435e34bf2
KB <kaabutu@gmail.com>
2022-05-14 10:53:07 +0000

    bug fixes

--- configurator/twitter_api.py
+++ configurator/twitter_api.py
@@ -9,20 +9,20 @@ class Configurator(object):
         self.setTwitterCredentials()
 
     def setTwitterCredentials(self):
-        # self.twitter_config = {
-        #     'base_url': os.environ["twitter_base_url"],
-        #     'app_key': self.param_store_agent.getParameter(os.environ["consumer_key"]),
-        #     'app_secret': self.param_store_agent.getParameter(os.environ["consumer_secret"]),
-        #     'oauth_token': self.param_store_agent.getParameter(os.environ["access_token"]),
-        #     'oauth_token_secret': self.param_store_agent.getParameter(os.environ["token_secret"])
-        # }
         self.twitter_config = {
             'base_url': os.environ["twitter_base_url"],
-            'app_key': os.environ["consumer_key"],
-            'app_secret': os.environ["consumer_secret"],
-            'oauth_token':os.environ["access_token"],
-            'oauth_token_secret': os.environ["token_secret"]
+            'app_key': self.param_store_agent.getParameter(os.environ["consumer_key"]),
+            'app_secret': self.param_store_agent.getParameter(os.environ["consumer_secret"]),
+            'oauth_token': self.param_store_agent.getParameter(os.environ["access_token"]),
+            'oauth_token_secret': self.param_store_agent.getParameter(os.environ["token_secret"])
         }
+        # self.twitter_config = {
+        #     'base_url': os.environ["twitter_base_url"],
+        #     'app_key': os.environ["consumer_key"],
+        #     'app_secret': os.environ["consumer_secret"],
+        #     'oauth_token':os.environ["access_token"],
+        #     'oauth_token_secret': os.environ["token_secret"]
+        # }
 
     def getTwitterCredentials(self):
         return self.twitter_config
